93e2dcd17a6a277969fbb07a30c3663ace65761f
Collins Evulukwu <siranaba@Collinss-MacBook-Air.local>
2019-10-09 12:05:10 -0500

    server updated

--- server/index.js
+++ server/index.js
@@ -4,3 +4,12 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const massive = require
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
